25fe9911e4c04c0af9242aa65c561c881149b98c
Olivier <olivierivard@hotmail.com>
2020-08-06 16:40:31 -0400

    Push de la correction

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {BrowserRouter as Router} from "react-router-dom"
+
+import App from "pages/index"
+
+    <header />
+    <Router />
+    <footer />
+
+}
+ReactDOM.render(<App />, document.getElementById('root'))
