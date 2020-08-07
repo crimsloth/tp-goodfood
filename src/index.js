2834e5b5cf7900ec94bb04f68354c357d9d3b078
Olivier <olivierivard@hotmail.com>
2020-08-06 20:07:15 -0400

    Router fonctionne après une capsule T1Gu1-Sensei!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {BrowserRouter as Router} from "react-router-dom"
+import {Link, Switch, Route} from "react-router-dom"
 
-import App from "pages/index"
+import Home from "./pages/home"
+import Test from "./pages/test"
 
-    <header />
-    <Router />
-    <footer />
-
+function App() {    
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/test">Test</Link>
+            
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/test">
+                    <Test />
+                </Route>
+            </Switch>
+        </div>
+    )
 }
-ReactDOM.render(<App />, document.getElementById('root'))
+
+ReactDOM.render(<Router><App /></Router>,document.getElementById("root"))
--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-function Home() {
+function Test() {
     return (
         <h1>Test</h1>
     )
 }
 
-export default Home
+export default Test
