f3beb834b580a341ac211ad108dd6d205fdc0f2d
Olivier <olivierivard@hotmail.com>
2020-08-24 15:18:55 -0400

    stage pour essayer de fix mon backup

--- src/index.js
+++ src/index.js
@@ -5,21 +5,27 @@ import {Link, Switch, Route} from "react-router-dom"
 
 import Home from "./pages/home"
 import Test from "./pages/test"
+import NavBar from "./components/header/navbar"
 
 function App() {    
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/test">Test</Link>
-            
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="/test">
-                    <Test />
-                </Route>
-            </Switch>
+            <div>
+                <NavBar />
+            </div>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/test">Test</Link>
+                
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/test">
+                        <Test />
+                    </Route>
+                </Switch>
+            </div>
         </div>
     )
 }
