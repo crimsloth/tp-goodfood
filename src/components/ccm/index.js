4c847c5541bf4208fdb2593609beabec5d002b2c
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2020-09-24 22:33:52 -0400

    Fix CCM & button semantic & accessibility

---
+++ src/components/ccm/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Button from "../button"
+import "./index.scss"
+
+function Ccm(props) {
+	return (
+		<div className="ccm">
+			<div className="ccm__content">
+				<div className="ccm__head">
+					{props.header}
+				</div>
+			</div>
+			<div className="ccm__fiches">
+				{props.children}
+			</div>
+		</div>
+	)
+}
+
+export default Ccm
